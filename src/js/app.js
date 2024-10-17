2beb01be98c755c7905d9e01fac587ce04917669
Stepan Stadniuk <stepanigcom@gmail.com>
2024-10-17 08:51:37 +0300

    + adaptation for mobile screens

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,15 @@
-import BaseHelpers from './helpers/BaseHelpers.js';
-
-BaseHelpers.checkWebpSupport();
-
-//spoilers
-import * as modules from './modules/modules';
-modules.spollers();
-
-//menu
-modules.menuInit();
-
-//popup
-import PopupManager from './modules/PopupManager';
-new PopupManager();
-
-//validation
-import './libs/jquery.maskedinput.min';
-jQuery(function ($) {
-  $("#telephone").mask("+38 (999) 999-9999");
-  $("#popup-telephone").mask("+38 (999) 999-9999");
+document.addEventListener('DOMContentLoaded', () => {
+  const aside = document.querySelector('.aside');
+  
+  const tabletScreen = window.matchMedia("(max-width: 1024px)")
+  tabletScreen.addEventListener("change", function() {
+    if (tabletScreen.matches) {
+      aside.classList.add('aside_variant_small');
+    } else {
+      aside.classList.remove('aside_variant_small');
+    }
+  });
+  aside.addEventListener('click', () => {
+    aside.classList.toggle('aside_variant_small');
+  });
 });
-
-//catalog
-import './modules/dynamicCatalog.js';
-
-//tabs
-modules.tabs();
-
-//gallery
-import './modules/galleryManager';
-
-//form handler
-import './modules/formHandler.js';
